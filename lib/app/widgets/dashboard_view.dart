d2e4ae0f208c472e26d1ed862b96c0f9778cebf6
Lucas Freitas <lucas.antunes@fiergs.org.br>
2022-07-31 23:37:13 -0300

    Adding resposive ui in cards

--- lib/app/widgets/dashboard_view.dart
+++ lib/app/widgets/dashboard_view.dart
@@ -11,42 +11,86 @@ import 'package:get/get.dart';
 class DashboardView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        TitleDashboard(text: "Resumo da atividade"),
-        Obx(
-          () => Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              CardDashboard(
-                icon: LoadIconSvg(IconsSvg.DEMAND),
-                title: '${controller.ordersCount} Pedidos',
-                subtitle: Currency.convertToBRL(controller.ordersTotal.value),
-                color: Color(0xffF4C8E1),
-              ),
-              CardDashboard(
-                icon: LoadIconSvg(IconsSvg.SALES),
-                title: '${controller.salesCount} Vendas',
-                subtitle: Currency.convertToBRL(controller.salesTotal.value),
-                color: Color(0xffB6EEDD),
-              ),
-              CardDashboard(
-                icon: LoadIconSvg(IconsSvg.AVERAGE_TICKET),
-                title: 'Ticket Médio',
-                subtitle: Currency.convertToBRL(controller.averageTicket.value),
-                color: Color(0xffC3E7F3),
-              ),
-            ],
-          ),
-        ),
-        TitleDashboard(text: "Pedidos"),
-        TableDashboard(),
-        Pagination(          
-          pagesPerRow: [05, 06, 07, 08, 09, 10, 15, 20],
-        ),
-      ],
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TitleDashboard(
+                text: "Resumo da atividade" + constraints.maxWidth.toString()),
+            Obx(
+              () => constraints.maxWidth > 1100
+                  ? Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CardDashboard(
+                          icon: LoadIconSvg(IconsSvg.DEMAND),
+                          title: '${controller.ordersCount} Pedidos',
+                          subtitle: Currency.convertToBRL(
+                              controller.ordersTotal.value),
+                          color: Color(0xffF4C8E1),
+                        ),
+                        CardDashboard(
+                          icon: LoadIconSvg(IconsSvg.SALES),
+                          title: '${controller.salesCount} Vendas',
+                          subtitle: Currency.convertToBRL(
+                              controller.salesTotal.value),
+                          color: Color(0xffB6EEDD),
+                        ),
+                        CardDashboard(
+                          icon: LoadIconSvg(IconsSvg.AVERAGE_TICKET),
+                          title: 'Ticket Médio',
+                          subtitle: Currency.convertToBRL(
+                              controller.averageTicket.value),
+                          color: Color(0xffC3E7F3),
+                        ),
+                      ],
+                    )
+                  : Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: CardDashboard(
+                            icon: LoadIconSvg(IconsSvg.DEMAND),
+                            title: '${controller.ordersCount} Pedidos',
+                            subtitle: Currency.convertToBRL(
+                                controller.ordersTotal.value),
+                            color: Color(0xffF4C8E1),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: CardDashboard(
+                            icon: LoadIconSvg(IconsSvg.SALES),
+                            title: '${controller.salesCount} Vendas',
+                            subtitle: Currency.convertToBRL(
+                                controller.salesTotal.value),
+                            color: Color(0xffB6EEDD),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: CardDashboard(
+                            icon: LoadIconSvg(IconsSvg.AVERAGE_TICKET),
+                            title: 'Ticket Médio',
+                            subtitle: Currency.convertToBRL(
+                                controller.averageTicket.value),
+                            color: Color(0xffC3E7F3),
+                          ),
+                        ),
+                      ],
+                    ),
+            ),
+            TitleDashboard(text: "Pedidos"),
+            TableDashboard(),
+            Pagination(
+              pagesPerRow: [05, 06, 07, 08, 09, 10, 15, 20],
+            ),
+          ],
+        );
+      },
     );
   }
 }
